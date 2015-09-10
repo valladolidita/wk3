e2beabd6271c45551ed2d6ff7cba0f2ff430f4e9
valladolidita <kelly@Kellys-MacBook-Pro.local>
2015-09-10 16:00:58 -0500

    Assignment 3
    Week 3 assignment for MCAD’s javascript class

---
+++ assignment3/script.js
@@ -0,0 +1,10 @@
+/*
+  Don't worry about the getElementById or addEventListener commands at this point,
+  we'll cover them more in-depth later in the course.
+  For the purposes of this assignment, you only need to understand that these commands
+  are used to respond to user interaction with the "Donate" button.
+*/
+document.getElementById('BtnDonate').addEventListener('click', function(){
+  //Code in here executes when the user clicks the "Donate" button.
+  	console.log("Hello World!");
+});
